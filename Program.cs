c5d561043ddd0e5ff7c99312df71b8d04344697a
Mohamed islem Bouazizi <mibtn10@gmail.com>
2024-01-29 12:54:15 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.NetworkInformation; //Include this
+
+
+namespace PingProto
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "TAV";
+            try
+            {
+                Ping myPing = new Ping();
+                PingReply reply = myPing.Send("10.82.131.250", 1000);
+                if (reply != null)
+                {
+                    Console.WriteLine("Address : " + reply.Address + "\nStatus :  " + reply.Status);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("ERROR: You have Some TIMEOUT issue");
+            }
+            Console.ReadKey();
+        }
+    }
+}
