9252aaccb4ae043ae17d66b2f50be66e1fce07f5
Mohamed islem Bouazizi <mibtn10@gmail.com>
2024-06-26 14:20:34 +0300

    Ip list update

--- Program.cs
+++ Program.cs
@@ -5,26 +5,38 @@ using System.Text;
 using System.Net.NetworkInformation; //Include this
 
 
+
 namespace PingProto
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.Title = "TAV";
-            try
+            var lines = System.IO.File.ReadAllLines("c:\\IP\\iplists.txt").Select(x => x.Split('/'));
+            Dictionary<string, string> IPS = new Dictionary<string, string>();
+            foreach (var line in lines)
+                IPS.Add( line[0], line[1]);
+        Console.Title = "TAV";
+            foreach (KeyValuePair<string, string> m in IPS)
             {
-                Ping myPing = new Ping();
-                PingReply reply = myPing.Send("10.82.131.250", 1000);
-                if (reply != null)
-                {
-                    Console.WriteLine("Address : " + reply.Address + "\nStatus :  " + reply.Status);
-                }
+                    Ping myPing = new Ping();
+                    PingReply reply = myPing.Send(m.Value, 1000);
+                    if (reply != null)
+                    {
+                    if (reply.Status == IPStatus.Success)
+                        Console.WriteLine("Address : " + reply.Address + "\nStatus :  Online");
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Address : " + reply.Address + "\nStatus :  Offline");
+                        Console.ResetColor();
+                    }
+                    }
             }
-            catch
-            {
-                Console.WriteLine("ERROR: You have Some TIMEOUT issue");
-            }
+            Console.WriteLine("Done");
+
+
             Console.ReadKey();
         }
     }
